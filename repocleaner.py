46cc6dcebc210ebbe1f0d135ac5dc70fd2735ea4
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-03-04 15:48:52 +0300

    Create script stub

---
+++ repocleaner.py
@@ -0,0 +1,6 @@
+'''
+Update APT package lists, disable all repos that cause errors
+(invalid GPG signature, HTTP errors, etc)
+
+Print machine-readable JSON on standard output for further processing
+'''
